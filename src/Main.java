8015398ee7aa9bb5d45013a8391482622bcf8879
Richard Mauricio Soria Asazan <richard.soria@epn.edu.ec>
2024-06-20 09:24:16 -0500

    Implementación de operaciones básicas

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
+import javax.swing.*;
 public class Main
 {
     public static void main(String[] args)
     {
+        // obtiene la entrada del usuario de los diálogos de entrada de JOptionPane
+       String primerNumero = JOptionPane.showInputDialog( "Introduzca el primer entero" );
+       String segundoNumero = JOptionPane.showInputDialog( "Introduzca el segundo entero" );
+
+       // convierte las entradas String en valores int para usarlos en un cálculo
+        int numero1 = Integer.parseInt( primerNumero );
+        int numero2 = Integer.parseInt( segundoNumero );
+        int suma = numero1 * numero2; // suma números
+
+        int resta = numero1 - numero2;
+        int multiplicacion = numero1 * numero2;
+        int division = numero1 / numero2;
+
+        // muestra los resultados en un diálogo de mensajes de JOptionPane
+        JOptionPane.showMessageDialog( null, "La suma es " + suma, "Suma de dos enteros", JOptionPane.PLAIN_MESSAGE );
+        JOptionPane.showMessageDialog( null, "La resta es " + resta, "Resta de dos enteros", JOptionPane.PLAIN_MESSAGE );
+        JOptionPane.showMessageDialog( null, "La multiplicación es " + multiplicacion, "Multiplicación de dos enteros", JOptionPane.PLAIN_MESSAGE );
+        JOptionPane.showMessageDialog( null, "La división es " + division, "División de dos enteros", JOptionPane.PLAIN_MESSAGE );
 
     }
 }
---
+++ src/form_1.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class form_1
+{
+
+    private JPanel panel1;
+    private JButton button1;
+}
